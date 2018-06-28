46820a0c0d7640deafcb18e7f54545844bae12ea
raowei <raowei@88bank.com>
2018-06-28 19:18:57 +0800

    chapter 8

---
+++ scala-learn-code/src/promgamming/8-foreach-script.scala
@@ -0,0 +1,32 @@
+
+List(1,2,3,4,5) foreach {
+  i => println("Int : " + i)
+}
+
+val stateCapitals = Map(
+  "Alabama" -> "Montgomery",
+  "Alaska" ->"Juneau",
+  "Wyoming" ->"Cheyenne"
+)
+
+stateCapitals foreach {
+  kv => println(kv._1 + " : " +kv._2)
+}
+
+val map2 = stateCapitals map { kv => (kv._1,kv._2.length)}
+
+val lengths = Map[String,Int]() ++ map2
+
+println(lengths)
+
+println(map2)
+
+val graph = List("a",List("b1","b2","b3"),List("c1",Nil,"c2"),Nil,"e")
+
+def flatten(list: List[_]) : List[_] = list flatMap {
+  case head :: tail => head::flatten(tail)
+  case Nil => Nil
+  case x => List(x)
+}
+
+println(flatten(graph))
